6950f005e04e0fd8675b2303019d055b03f07b9e
Summer Mueller <162736441+SummerMueller@users.noreply.github.com>
2024-07-01 09:46:50 -0400

    Update and rename ExtractKnowledge.java to Reasoning.java
    Added dangling node validation

--- Reasoning.java
+++ Reasoning.java
@@ -7,13 +7,15 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class ExtractKnowledge {
+public class Reasoning {
     
    public static record Block(String name, String xmi) {
    }
     
-   public static record Port(String name, String xmi, String xmiOfOwner) {
+   public static record Port(String name, String xmi, String xmiOfOwner, 
+         String reusesProperty) {
    }
 
    public static record Association(String name, String xmi, String sourceName, 
@@ -23,13 +25,23 @@ public class ExtractKnowledge {
    public static record ClassifierRole(String name, String xmi, String type, 
          String xmiOfOwner, String propertyType, String reusesProperty) {
    }
+   
+   public static record Function(String name, String xmi, String xmiOfOwner) {
+   }
+   
+   public static record Error(String type, String portName, String xmiOfPort, 
+         String blockName, String xmiOfBlock) {
+   }
 
    public static void main(String[] args) {
 
       try {
-
+         
          // Sets up the file
-         File inputFile = new File("data/CoffeeMaker.xml");
+         //File inputFile = new File("C:\\Users\\summe\\Functional Reasoning\\ActiveStandby_NLP.xml");
+         //File inputFile = new File("C:\\Users\\summe\\Functional Reasoning\\CoffeeMaker_NLP.xml");
+         //File inputFile = new File("C:\\Users\\summe\\Functional Reasoning\\FGS_NLP.xml");
+         File inputFile = new File("C:\\Users\\summe\\Functional Reasoning\\HairDryer_NLP.xml");
          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
          Document doc = dBuilder.parse(inputFile);
@@ -70,9 +82,20 @@ public class ExtractKnowledge {
                } else if (type.equals("Port")) {
                   Node taggedValueNode2 = taggedValueList.item(2);
                   Element taggedValueElement2 = (Element) taggedValueNode2;
+                     
+                  // Determines if the port has a reuses property
+                  String reusesProperty;
+                  if (taggedValueList.getLength() >= 5) {
+                     Node taggedValueNode4 = taggedValueList.item(4);
+                     Element taggedValueElement4 = (Element) taggedValueNode4;
+                     reusesProperty = taggedValueElement4.getAttribute("value");
+                  } else {
+                     reusesProperty = "";
+                  }
                   allPorts.add(new Port (classElement.getAttribute("name"), 
                         classElement.getAttribute("xmi.id"),
-                        taggedValueElement2.getAttribute("value")));
+                        taggedValueElement2.getAttribute("value"), 
+                        reusesProperty));
                } else {
                   System.out.printf ("Invalid UML:Class tag at %s%n", 
                         classElement.getAttribute("name"));
@@ -112,7 +135,7 @@ public class ExtractKnowledge {
                   aEndElement1.getAttribute("type")));
          }
          
-         // Creates an ArrayList of records to old all the classifier roles
+         // Creates an ArrayList of records to hold all the classifier roles
          List<ClassifierRole> allRoles = new ArrayList<ClassifierRole>();
          
          NodeList roleList = namespaceElement.getElementsByTagName("UML:ClassifierRole");
@@ -151,18 +174,97 @@ public class ExtractKnowledge {
                   reusesProperty));
             
          }
+         
+         // Creates an ArrayList of records to hold all the functions
+         List<Function> allFunctions = new ArrayList<Function>();
+         
+         NodeList functionList = doc.getElementsByTagName("UML:TaggedValue");
+         
+         // Iterates over each UML:TaggedValue
+         for (int i = 0; i < functionList.getLength(); i++) {
+            
+            Node functionNode = functionList.item(i);
+            Element functionElement = (Element) functionNode;
+            
+            // Verifies the tag is a function
+            String tag = functionElement.getAttribute("tag");
+            if (tag.length() > 4) {
+               if (tag.substring(0,4).equals("Func")) {
+                  
+                  // Adds a new function to the list of all functions
+                  allFunctions.add(new Function (functionElement.getAttribute("value").trim(),
+                        functionElement.getAttribute("xmi.id"),
+                        functionElement.getAttribute("modelElement")));
+               }
+            }
+         }     
 
          // Tests by printing the first element of each list
-         System.out.println(allBlocks.get(0));
+         /*System.out.println(allBlocks.get(0));
          System.out.println();
          System.out.println(allPorts.get(0));
          System.out.println();
          System.out.println(allAssociations.get(0));
          System.out.println();
          System.out.println(allRoles.get(0));
+         System.out.println();
+         System.out.println(allFunctions.get(0));*/
+         
+
+         // Begins validating the dangling node inspection
+         
+         // Creates an ArrayList to hold the complete list of errors and their types
+         List<Error> allErrors = new ArrayList<Error>();
+         
+         // Creates two HashMaps to separately define the sources and destinations of each association
+         HashMap<String, String> sources = new HashMap<String, String>();
+         HashMap<String, String> destinations = new HashMap<String, String>();
+         
+         // Fills the maps with unique associations but not necessarily unique ports 
+         for (Association association : allAssociations) {
+           sources.put(association.xmi(), association.xmiOfSource());
+           destinations.put(association.xmi(), association.xmiOfDestination());
+         }
+   
+         // Validates that each port within an IBD is either the source or destination of an association
+         for (Port port : allPorts) {
+            if (!(port.reusesProperty().equals(""))) {
+               if (!(sources.containsValue(port.xmi()) || destinations.containsValue(port.xmi()))) {
+                  String ownerName = "";
+                  // Finds the name of the owner's block
+                  for (ClassifierRole role : allRoles) {
+                     if (role.xmi().equals(port.xmiOfOwner())) {
+                        ownerName = role.name();
+                     }
+                  }
+                  allErrors.add(new Error ("Dangling Node", port.name(), port.xmi(), 
+                        ownerName, port.xmiOfOwner()));
+               }     
+            }
+         }
+
+         // Prints the type of each error caught and its location
+         for (Error error : allErrors) {
+            System.out.printf ("%s%n%n", error);
+         }
+         System.out.println(allPorts.size());
+         System.out.println(allErrors.size());
+         
 
       } catch (Exception e) {
          e.printStackTrace();
       }
    }
+   
+   /*public static String getOwnerName (final String xmiOfOwner, final List list) {
+      String ownerName = "";
+      for (int i = 0; i < list.size(); i++) {
+         ClassifierRole role = list.get(i);
+         if (role.xmi().equals(xmiOfOwner)) {
+            ownerName = role.name();
+            return ownerName;
+         }
+      }
+      return ownerName;
+   }*/
 }
